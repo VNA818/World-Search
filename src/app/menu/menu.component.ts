06fde43735e81fe30f65adcbad76fd7813e1dca4
VNA818-RPI <Arcerv@rpi.edu>
2022-03-20 16:33:00 -0400

    changed menu links to a tags changed logo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,18 +9,6 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
-  home() {
-    window.open("https://vna818.github.io/world-search");
-  }
-
-  about() {
-    window.open("https://github.com/VNA818/world-search/blob/main/README.md", '_blank');
-  }
-
-  repo () {
-    window.open("https://github.com/VNA818/world-search", '_blank');
-  }
-
   ngOnInit(): void {
   }
 
